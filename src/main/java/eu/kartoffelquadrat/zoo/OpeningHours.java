8021c53c63070e38b26159c19f005aef38ed6a66
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-21 13:55:37 -0400

    Added google checkstyle verification

--- src/main/java/eu/kartoffelquadrat/zoo/OpeningHours.java
+++ src/main/java/eu/kartoffelquadrat/zoo/OpeningHours.java
@@ -7,46 +7,46 @@ package eu.kartoffelquadrat.zoo;
  */
 public class OpeningHours {
 
-    // immutable fields for hard coded opening hours.
-    private final String MON_FRI = "9AM - 7PM";
-    private final String SAT_SUN = "10AM - 8PM";
+  // immutable fields for hard coded opening hours.
+  private static final String MON_FRI = "9AM - 7PM";
+  private static final String SAT_SUN = "10AM - 8PM";
 
-    /**
-     * Default constructor.
-     */
-    public OpeningHours() {
-    }
+  /**
+   * Default constructor.
+   */
+  public OpeningHours() {
+  }
 
-    /**
-     * Getter for opening hours on weekdays.
-     *
-     * @return String encoding opening hours Mondays to Fridays.
-     */
-    public String getMonFri() {
-        return MON_FRI;
-    }
+  /**
+   * Getter for opening hours on weekdays.
+   *
+   * @return String encoding opening hours Mondays to Fridays.
+   */
+  public String getMonFri() {
+    return MON_FRI;
+  }
 
-    /**
-     * Getter for opening hours on weekends.
-     *
-     * @return String encoding opening hours Saturdays and Sundays.
-     */
-    public String getWeekEnd() {
-        return SAT_SUN;
-    }
+  /**
+   * Getter for opening hours on weekends.
+   *
+   * @return String encoding opening hours Saturdays and Sundays.
+   */
+  public String getWeekEnd() {
+    return SAT_SUN;
+  }
 
-    /**
-     * Custom toString method for convenient string representation of open hours object.
-     *
-     * @return all field information of this open hours object formatted as human readable string.
-     */
-    @Override
-    public String toString() {
-        StringBuilder openingHoursBuilder = new StringBuilder("");
-        openingHoursBuilder.append("\tMon-Fri: ")
-                .append(getMonFri())
-                .append("\n\tSat/Sun: ")
-                .append(getWeekEnd());
-        return openingHoursBuilder.toString();
-    }
+  /**
+   * Custom toString method for convenient string representation of open hours object.
+   *
+   * @return all field information of this open hours object formatted as human readable string.
+   */
+  @Override
+  public String toString() {
+    StringBuilder openingHoursBuilder = new StringBuilder("");
+    openingHoursBuilder.append("\tMon-Fri: ")
+        .append(getMonFri())
+        .append("\n\tSat/Sun: ")
+        .append(getWeekEnd());
+    return openingHoursBuilder.toString();
+  }
 }
--- src/main/java/eu/kartoffelquadrat/zoo/Zoo.java
+++ src/main/java/eu/kartoffelquadrat/zoo/Zoo.java
@@ -5,79 +5,80 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Principal class of the Zoo, offering all relevant public functionality. Access to public methods of this implementation
- * is demonstrated in the DesktopLauncher class. This class is a singleton.
+ * Principal class of the Zoo, offering all relevant public functionality. Access to public methods
+ * of this implementation is demonstrated in the DesktopLauncher class. This class is a singleton.
  *
  * @author Maximilian Schiedermeier
  */
 public class Zoo {
 
-    // Singleton reference
-    private static Zoo SINGLETON_REFERENCE = null;
+  // Singleton reference
+  private static Zoo SINGLETON_REFERENCE = null;
 
-    // private fields for indexed animals and opening hours.
-    private final Map<String, Animal> animals;
-    private final OpeningHours openingHours;
+  // private fields for indexed animals and opening hours.
+  private final Map<String, Animal> animals;
+  private final OpeningHours openingHours;
 
-    /**
-     * Private constructor, populates the zoo.
-     */
-    private Zoo() {
-        openingHours = new OpeningHours();
-        animals = new LinkedHashMap<>();
-        addAnimal("Charly", new Animal("Chimpanzee", 10, "Bananas"));
-        addAnimal("Nemo", new Animal("Clownfish", 4, "Plankton"));
-        addAnimal("Dumbo", new Animal("Elephant", 13, "Peanuts"));
+  /**
+   * Private constructor, populates the zoo.
+   */
+  private Zoo() {
+    openingHours = new OpeningHours();
+    animals = new LinkedHashMap<>();
+    addAnimal("Charly", new Animal("Chimpanzee", 10, "Bananas"));
+    addAnimal("Nemo", new Animal("Clownfish", 4, "Plankton"));
+    addAnimal("Dumbo", new Animal("Elephant", 13, "Peanuts"));
+  }
+
+  /**
+   * Singleton patter access method.
+   *
+   * @return the one and only Zoo instance.
+   */
+  public static Zoo getInstance() {
+    if (SINGLETON_REFERENCE == null) {
+      SINGLETON_REFERENCE = new Zoo();
     }
+    return SINGLETON_REFERENCE;
+  }
 
-    /**
-     * Singleton patter access method.
-     *
-     * @return the one and only Zoo instance.
-     */
-    public static Zoo getInstance() {
-        if (SINGLETON_REFERENCE == null)
-            SINGLETON_REFERENCE = new Zoo();
-        return SINGLETON_REFERENCE;
-    }
+  /**
+   * Returns a list with all animal names.
+   *
+   * @return Collection with the names of all indexed animals.
+   */
+  public Collection<String> getAllAnimalNames() {
+    return animals.keySet();
+  }
 
-    /**
-     * Returns a list with all animal names.
-     *
-     * @return Collection with the names of all indexed animals.
-     */
-    public Collection<String> getAllAnimalNames() {
-        return animals.keySet();
-    }
+  /**
+   * Getter to look up details on one specific animal, identified by name.
+   *
+   * @param name as the animals name.
+   * @return the animal object associated to this name.
+   */
+  public Animal getAnimalDetails(String name) {
+    return animals.get(name);
+  }
 
-    /**
-     * Getter to look up details on one specific animal, identified by name.
-     *
-     * @param name as the animals name.
-     * @return the animal object associated to this name.
-     */
-    public Animal getAnimalDetails(String name) {
-        return animals.get(name);
-    }
+  /**
+   * Setter to add a new animal to the zoo.
+   *
+   * @param name   as the name of the animal to index.
+   * @param animal as the characteristics of the animal to add.
+   */
+  public void addAnimal(String name, Animal animal) {
+    animals.put(name, animal);
+  }
 
-    /**
-     * Setter to add a new animal to the zoo.
-     *
-     * @param name   as the name of the animal to index.
-     * @param animal as the characteristics of the animal to add.
-     */
-    public void addAnimal(String name, Animal animal) {
-        animals.put(name, animal);
-    }
-
-    /**
-     * Getter to retrieve the zoo opening hours.
-     *
-     * @return the zoo opening hours.
-     */
-    public OpeningHours getOpeningHours() {
-        return openingHours;
-    }
+  /**
+   * Getter to retrieve the zoo opening hours.
+   *
+   * @return the zoo opening hours.
+   */
+  public OpeningHours getOpeningHours() {
+    return openingHours;
+  }
 
 
 }
